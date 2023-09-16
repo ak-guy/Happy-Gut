6dc788a7b11e89b71b745ecd2ec5ed9d16f3c445
Arpit <arpitkumar3203@gmail.com>
2023-09-16 23:15:16 +0530

    activating the user on token verification

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,8 @@ from accounts.utils import detectUser, send_verification_mail
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 def check_role_vendor(user):
     '''
@@ -114,10 +116,26 @@ def registerVendor(request):
     }
     return render(request, 'accounts/registerVendor.html', context)
 
-def activate(request, uidb, token):
-    '''Activate the user by setting is_active to true
+def activate(request, uidb64, token):
     '''
-    return None
+    Activate the user by setting is_active to true
+    '''
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user and default_token_generator.check_token(user, token):
+        user.is_active =True
+        user.save()
+        messages.success(request, "Congratulation your account is activated")
+        print("valid action link")
+        return redirect('myAccount')
+    else:
+        messages.error(request, "Invalid activation Link")
+        return redirect('myAccount')
+    
 
 def login(request):
     if request.user.is_authenticated:
