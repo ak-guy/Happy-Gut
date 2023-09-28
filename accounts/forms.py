c963cdaeb8d71f8781d99cc82caba6af9d31154b
Arpit <arpitkumar3203@gmail.com>
2023-09-28 18:56:58 +0530

    setting up restaurant profile form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -16,3 +16,8 @@ class UserForm(forms.ModelForm):
 
         if password != confirm_password:
             raise forms.ValidationError("Password does not match!")
+        
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'city', 'state', 'country', 'pin_code', 'latitude', 'longitude']
--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
+from django.contrib import messages
+
+from accounts.models import UserProfile
+from .models import Vendor
 
 # Create your views here.
 def vprofile(request):
-    return render(request, "vendors/vprofile.html")
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
+
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Settings has been updated')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+            messages.error(request, 'Settings has not been updated')
+    else:
+        profile_form = UserProfileForm(instance=profile) # passing instance will help form load the existing data
+        vendor_form = VendorForm(instance=vendor) # passing instance will help form load the existing data
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    return render(request, "vendors/vprofile.html", context=context)
